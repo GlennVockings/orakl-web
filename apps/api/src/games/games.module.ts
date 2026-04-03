9d47d63f05f92d6d34822e2080e2b4042e555a6a
Glenn <glenn@fxdigital.uk>
2026-04-03 23:55:44 +0100

    fix: web socket gateway provider added

--- apps/api/src/games/games.module.ts
+++ apps/api/src/games/games.module.ts
@@ -3,10 +3,11 @@ import { GamesController } from './games.controller';
 import { GamesService } from './games.service';
 import { PrismaService } from '../prisma.service';
 import { GameAccessService } from './game-access.service';
+import { WsGateway } from 'src/ws/ws.gateway';
 
 @Module({
   controllers: [GamesController],
-  providers: [GamesService, PrismaService, GameAccessService],
+  providers: [GamesService, PrismaService, GameAccessService, WsGateway],
   exports: [GameAccessService],
 })
 export class GamesModule {}
