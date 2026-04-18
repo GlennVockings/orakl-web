e4264fb9415b460ec3ba3091d3afab546726a7db
Glenn <glennvockings@gmail.com>
2026-04-19 00:50:32 +0100

    fix: sets node version

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -15,7 +15,8 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ? Number(process.env.PORT) : 3001);
+  const port = process.env.PORT ? Number(process.env.PORT) : 3001;
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
