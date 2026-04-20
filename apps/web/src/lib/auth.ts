258258b98b7329e27759d0132401c1d5b3c813bf
Glenn <glennvockings@gmail.com>
2026-04-20 09:29:25 +0100

    fix: updated schema

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -34,6 +34,7 @@ export const auth = betterAuth({
   // Helps with cookies/cors when calling /api/auth from browser
   trustedOrigins: [
     "http://localhost:3000",
-    "http://192.168.1.243:3000"
+    "http://192.168.1.243:3000",
+    "https://faux-stakes-web.vercel.app"
   ],
 });
